41afaddea30f4fda0b8eb14e3946d38d67089d91
Jose Cruz <jcgtcryguz@gmail.com>
2026-03-04 21:15:37 -0600

    se agrego buscador de directorios y archivos en disco interno

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
 require("dotenv").config();
 
-const express = require("express");
+const express = require('express');
+const fs = require('fs');
 const path = require('path');
+
 const app = express();
 
+// 0. Define el librerias agregadas a utilizar por express
 app.use(express.static(path.join(__dirname, 'pagina_web')));
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname,'pagina_web','index.html'));
-});
-// Endpoint que recibirá la petición GET
-app.get('/mensaje', (req, res) => {
-    // req.query captura parámetros de la URL: /api/saludo?nombre=Juan
-    const nombre = req.query.nombre ;
-    res.json({
-        mensaje: `Hola, ${nombre}, un fuerte abrazo desde el Servidor`,
-        status: 'success'
-    });
+// 1. Servir el archivo HTML principal
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pagina_web/index.html'));
 });
 
+// 2. Endpoint para explorar el disco duro
+app.get('/explore', (req, res) => {
+    const inputPath = req.query.direccion;
+    if (!inputPath) return res.status(400).json({ error: 'Ruta vacía' });
+
+    try {
+        if (!fs.existsSync(inputPath)) return res.status(404).json({ error: 'No existe' });
+
+        const stats = fs.lstatSync(inputPath);
+
+        if (stats.isDirectory()) {
+            // Caso: Directorio
+            fs.readdir(inputPath, (err, files) => {
+                if (err) throw err;
+                res.json({ tipo: 'directorio', ruta: inputPath, contenido: files });
+            });
+        } else if (stats.isFile()) {
+            // Caso: Archivo (leído como texto UTF-8)
+            fs.readFile(inputPath, 'utf8', (err, data) => {
+                if (err) return res.status(500).json({ error: 'No se pudo leer el archivo' });
+                res.json({ tipo: 'archivo', ruta: inputPath, contenido: data });
+            });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Error de acceso al sistema' });
+    }
+});
 const PORT = process.env.PORT ;
-app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+});
